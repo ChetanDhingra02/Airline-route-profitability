93dece9278fc9cf345e6fcf5ca3a87040ee699a2
ChetanDhingra02 <114406088+ChetanDhingra02@users.noreply.github.com>
2026-05-06 03:56:21 -0600

    Update airapp.py

--- airapp.py
+++ airapp.py
@@ -10,7 +10,7 @@ import streamlit.components.v1 as components
 
 
 st.set_page_config(
-    page_title="Airline Profitability System",
+    page_title="Airlines Profitability System",
     page_icon="✈️",
     layout="wide",
     initial_sidebar_state="expanded"
