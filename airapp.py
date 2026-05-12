4e14bfe56e7eee82793e16e8021ec2d1be951d51
ChetanDhingra02 <114406088+ChetanDhingra02@users.noreply.github.com>
2026-05-12 02:59:10 -0600

    Update airapp.py

--- airapp.py
+++ airapp.py
@@ -759,7 +759,7 @@ html, body, [class*="css"] {{
 DECISION_COLORS = {
     "Expand":   CHART_EXPAND,
     "Maintain": CHART_MAINTAIN,
-    "Optimize": CHART_ORANGE,
+    "Optimize": CHART_OPTIMIZE,
     "Drop":     CHART_DROP,
 }
 ORDER = ["Expand", "Maintain", "Optimize", "Drop"]
@@ -999,7 +999,7 @@ with tab1:
         wc = {
             "Expand":   CHART_EXPAND,
             "Maintain": CHART_MAINTAIN,
-            "Optimize": CHART_ORANGE,
+            "Optimize": CHART_OPTIMIZE,
             "Drop":     CHART_DROP,
         }
         fig, ax = clean_fig((5, 4.6))
@@ -1245,7 +1245,7 @@ with tab4:
     bc_map = {
         "Expand":   CHART_EXPAND,
         "Maintain": CHART_MAINTAIN,
-        "Optimize": CHART_ORANGE,
+        "Optimize": CHART_OPTIMIZE,
         "Drop":     CHART_DROP,
     }
     bc = [bc_map.get(i, "#CCCCCC") for i in urbd.index]
@@ -1486,7 +1486,7 @@ with tab5:
         bar_cp = {
             "Expand":   CHART_EXPAND,
             "Maintain": CHART_MAINTAIN,
-            "Optimize": CHART_ORANGE,
+            "Optimize": CHART_OPTIMIZE,
             "Drop":     CHART_DROP,
         }
         fig, ax = clean_fig((6, 3.5))
